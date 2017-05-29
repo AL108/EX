3acaec5305c277cb6ec03c13e151e6a80e1fb27d
AL108 <alieberman108@gmail.com>
2017-05-30 06:38:26 +1000

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EX
+//
+//  Created by Ali Lieberman on 30/5/17.
+//  Copyright © 2017 Ali Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+        print("hi")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
